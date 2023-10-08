63d14ea14b967d03a11074c69312d5b04e00efa3
Paloma Cristina Faria <palomacfaria@outlook.com>
2023-10-08 10:57:26 -0300

    Organizando estrutura de um projeto

---
+++ Canvas/sptint-3/02-Organizando-Estrutura/src/scripts/data.js
@@ -0,0 +1,54 @@
+const data = [
+    {
+        name:'Caderno',
+        image:'/Canvas/sptint-3/02-Organizando-Estrutura/src/img/product.jpg',
+        price:'R$50,00',
+        off: 10
+    },
+    {
+        name:'Livro',
+        image:'/Canvas/sptint-3/02-Organizando-Estrutura/src/img/product.jpg',
+        price:'R$80,00',
+        off: 20
+    },{
+        name:'Apontador',
+        image:'/Canvas/sptint-3/02-Organizando-Estrutura/src/img/product.jpg',
+        price:'R$5,00',
+        off: 30
+    },{
+        name:'Lápis',
+        image:'/Canvas/sptint-3/02-Organizando-Estrutura/src/img/product.jpg',
+        price:'R$2,50',
+        off: 0
+    },{
+        name:'Tesoura',
+        image:'/Canvas/sptint-3/02-Organizando-Estrutura/src/img/product.jpg',
+        price:'R$6,00',
+        off: 10
+    },{
+        name:'Canetinhas',
+        image:'/Canvas/sptint-3/02-Organizando-Estrutura/src/img/product.jpg',
+        price:'R$15,00',
+        off: 50
+    },{
+        name:'Estojo',
+        image:'/Canvas/sptint-3/02-Organizando-Estrutura/src/img/product.jpg',
+        price:'R$25,00',
+        off: 0
+    },{
+        name:'Régua',
+        image:'/Canvas/sptint-3/02-Organizando-Estrutura/src/img/product.jpg',
+        price:'R$5,00',
+        off: 0
+    },{
+        name:'Mochila',
+        image:'/Canvas/sptint-3/02-Organizando-Estrutura/src/img/product.jpg',
+        price:'R$250,00',
+        off: 20
+    },{
+        name:'Giz de Cera',
+        image:'/Canvas/sptint-3/02-Organizando-Estrutura/src/img/product.jpg',
+        price:'R$18,00',
+        off: 80
+    },
+]
---
+++ Canvas/sptint-3/02-Organizando-Estrutura/src/scripts/footer.js
@@ -0,0 +1,7 @@
+function footerRender(){
+    const footer = document.querySelector('footer')
+
+    footer.id = 'footer'
+    footer.innerText = 'All rights reserved.'
+}
+footerRender();
