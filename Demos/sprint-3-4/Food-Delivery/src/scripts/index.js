192742bada4beecfa3eeb9b2800b798d54c7f3e9
Paloma Cristina Faria <palomacfaria@outlook.com>
2023-10-11 20:14:38 -0300

    Adicionando recursos do repositório base

---
+++ Demos/sprint-3-4/Food-Delivery/src/scripts/index.js
@@ -0,0 +1,48 @@
+const menu = [
+    { id: 1, name: "Hamburguer", price: 15, image: "./src/assets/burgers.svg" },
+    { id: 2, name: "Churrasco", price: 34.9, image: "./src/assets/steak.svg" },
+    {
+      id: 3,
+      name: "Frango assado",
+      price: 29.9,
+      image: "./src/assets/roast-chicken.svg",
+    },
+    {
+      id: 4,
+      name: "Feijoada",
+      price: 17.8,
+      image: "./src/assets/pork-and-beans.svg",
+    },
+    { id: 5, name: "Prato feito", price: 15, image: "./src/assets/lunch.svg" },
+    { id: 6, name: "Salada", price: 12, image: "./src/assets/salad.svg" },
+    {
+      id: 7,
+      name: "Macarrão de arroz",
+      price: 14.9,
+      image: "./src/assets/rice-noodle.svg",
+    },
+    {
+      id: 8,
+      name: "Hamburguer vegano",
+      price: 15,
+      image: "./src/assets/vegan-burger.svg",
+    },
+    {
+      id: 9,
+      name: "Salada de frutas",
+      price: 13.9,
+      image: "./src/assets/fruit-salad.svg",
+    },
+    { id: 10, name: "Sushi", price: 29.9, image: "./src/assets/sushi.svg" },
+    { id: 11, name: "Pastel", price: 12, image: "./src/assets/fried-pastry.svg" },
+    { id: 12, name: "Pizza", price: 35, image: "./src/assets/pizza.svg" },
+    {
+      id: 13,
+      name: "Bolo de morango",
+      price: 26.9,
+      image: "./src/assets/strawberry-cake.svg",
+    },
+    { id: 14, name: "Açaí", price: 7.9, image: "./src/assets/acai.svg" },
+    { id: 15, name: "Quindim", price: 19.9, image: "./src/assets/custard.svg" },
+    { id: 16, name: "Sorvete", price: 4.9, image: "./src/assets/icecream.svg" },
+  ];
