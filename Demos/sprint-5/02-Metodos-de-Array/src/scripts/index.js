e8f2bd356df9936cd0edd9e20c6d8ad40b33b4ac
Paloma Cristina Faria <palomacfaria@outlook.com>
2023-10-24 19:07:34 -0300

    ForEach / Map / Filter / Reduce

--- Demos/sprint-5/02-Metodos-de-Array/src/scripts/index.js
+++ Demos/sprint-5/02-Metodos-de-Array/src/scripts/index.js
@@ -44,4 +44,43 @@ const renderCards = (clientsArray) => {
     });
 }
 
+//*MAP -> Mapear array - executa uma callback em cada elemento e retorna um novo array com a execução.
+
+const listAverage = (clientsArray) => {
+
+    //const averageList = clientsArray.map((currentClient) => currentClient.age);
+
+    const averageList = clientsArray.map((currentClient) => {
+        return currentClient.age;
+    });
+
+    console.log(averageList);
+}
+
+//*FILTER -> Filtrar - executa uma callback com uma validação de dados, retornando um novo array baseado nos itens que passou na condicional.
+const filterByAge = (clientsArray) => {
+    const clientsByAge = clientsArray.filter((currentClient) => {
+        /* if(currentClient.age <= 30){
+            return true;
+        }else{
+            return false;
+        } */
+        return (currentClient.age > 30)
+    
+    });
+    console.log(clientsByAge);
+}
+
+//*REDUCE -> Reduzir - Reduz o nosso array e um único valor resultante de uma callback.
+const allAge = (clientsArray) => {
+    const result = clientsArray.reduce((acumulador, currentValue) => {
+        return acumulador + currentValue.age;
+    }, 0);
+
+    console.log(result / clientsArray.length);
+}
+
 renderCards(clients);
+listAverage(clients);
+filterByAge(clients);
+allAge(clients);
