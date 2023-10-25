bb5493e510f6786ed02d14ce2db4a3e556ca2792
Paloma Cristina Faria <palomacfaria@outlook.com>
2023-10-25 19:24:29 -0300

    Métodos de array demo 2

--- Demos/sprint-5/02-Metodos-de-Array/src/scripts/index.js
+++ Demos/sprint-5/02-Metodos-de-Array/src/scripts/index.js
@@ -1,130 +1,161 @@
 import { clients } from "./database.js";
 
 const createCard = (user) => {
-    //Criando elementos
-    const liCard = document.createElement('li');
-    const h2Name = document.createElement('h2');
-    const pEmail = document.createElement('p');
-    const pIdade = document.createElement('p');
-    const pTel = document.createElement('p');
-    const btnDelete = document.createElement('button');
+  const { name, email, age, phone } = user;
 
-    //Adicionando classe
-    liCard.classList.add('clients__card');
+  const liCard = document.createElement("li");
+  const h2Name = document.createElement("h2");
+  const pEmail = document.createElement("p");
+  const pIdade = document.createElement("p");
+  const pTel = document.createElement("p");
+  const btnDelete = document.createElement("button");
 
-    //Inserindo valores
-    h2Name.innerText = user.name;
-    pEmail.innerText = `Email: ${user.email}`;
-    pIdade.innerText = `Idade: ${user.age}`;
-    pTel.innerText = `Telefone: ${user.phone}`;
-    btnDelete.innerText = 'X';
+  liCard.classList.add("clients__card");
 
-    //Adicionando elementos a estrutura
-    liCard.append(h2Name, pEmail, pIdade, pTel, btnDelete);
+  h2Name.innerText = name;
+  pEmail.innerText = `Email: ${email}`;
+  pIdade.innerText = `Idade: ${age}`;
+  pTel.innerText = `Telefone: ${phone}`;
+  btnDelete.innerText = "X";
 
-    btnDelete.addEventListener('click', (event)=>{
-        //Achar a posição do objeto dentro do array
-        const index = clients.indexOf(user);
-        console.log(index);
+  btnDelete.addEventListener("click", (event) => {
+    const index = clients.indexOf(user);
+    clients.splice(index, 1);
 
-        //Remove o elemento dentro do array
-        clients.splice(index, 1);
+    // event.target.parentNode.remove();
 
-        //?Também é possível remover assim:
-        //event.target.parentNode.remove();
+    renderCards(clients);
+  });
 
-        //Renderiza novamente
-        renderCards(clients);
-    });
+  liCard.append(h2Name, pEmail, pIdade, pTel, btnDelete);
 
-    return liCard;
-}
+  return liCard;
+};
+
+// const usuario = {
+//   id: 3,
+//   name: "Amanda",
+//   email: "amanda@mail.com",
+//   age: 43,
+//   phone: "999666555",
+// };
+
+// const card = createCard(usuario);
+
+// console.log(card);
 
 const renderCards = (clientsArray) => {
-    const ulClients = document.querySelector('.clients__list');
+  const ulClients = document.querySelector(".clients__list");
 
-    ulClients.innerHTML = '';
+  ulClients.innerHTML = "";
 
-    /* for(let i = 0; i < clientsArray.length; i++){
-        const currentClient = clientsArray[i];
+  //   for (let i = 0; i < clientsArray.length; i++) {
+  //     const currentClient = clientsArray[i];
 
-        const card = createCard(currentClient);
+  //     const card = createCard(currentClient);
 
-        ulClients.appendChild(card);
-    } */
+  //     ulClients.appendChild(card);
+  //   }
 
-    //* forEach -> Para cada elemento do array executa uma função callback.
+  // forEach -> para cada - executa uma callback em cada elemento do meu array
 
-    clientsArray.forEach((currentClient)=>{
-        const card = createCard(currentClient);
-        ulClients.appendChild(card);
-    });
-}
-
-//*MAP -> Mapear array - executa uma callback em cada elemento e retorna um novo array com a execução.
+  clientsArray.forEach((currentClient) => {
+    const card = createCard(currentClient);
+    ulClients.appendChild(card);
+  });
+};
 
 const listAverage = (clientsArray) => {
+  // map -> mapear o array - executa uma callback em cada elemento e retorna um novo array com a execução
 
-    //const averageList = clientsArray.map((currentClient) => currentClient.age);
+  const averageList = clientsArray.map((currentClient) => currentClient.age);
+  //   const averageList = clientsArray.map((currentClient) => {
+  //     return currentClient.age
+  //   });
 
-    const averageList = clientsArray.map((currentClient) => {
-        return currentClient.age;
+  console.log(averageList);
+};
+
+const filterByAge = (clientsArray, overAge) => {
+  // filter -> filtrar - executa uma callbak com uma validação de dados,retornando um novo array baseado nos items que passou na condicional
+
+  // if (currentClient.age <= 30) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+  const clientsByAge = clientsArray.filter((currentClient) => {
+    if (overAge) {
+      return currentClient.age > 30;
+    } else {
+      return currentClient.age <= 30;
+    }
+  });
+
+  return clientsByAge;
+};
+
+const addFilterEvents = () => {
+  const searchInput = document.querySelector(".search__input");
+  const bellowBtn = document.querySelector("#filterBellow30");
+  const overBtn = document.querySelector("#filterOver30");
+
+  searchInput.addEventListener("keyup", (event) => {
+    const filtered = clients.filter((currentClient) => {
+      return currentClient.name
+        .toLowerCase()
+        .includes(event.target.value.toLowerCase());
     });
 
-    console.log(averageList);
-}
+    renderCards(filtered);
+  });
 
-//*FILTER -> Filtrar - executa uma callback com uma validação de dados, retornando um novo array baseado nos itens que passou na condicional.
-const filterByAge = (clientsArray) => {
-    const clientsByAge = clientsArray.filter((currentClient) => {
-        /* if(currentClient.age <= 30){
-            return true;
-        }else{
-            return false;
-        } */
-        return (currentClient.age > 30)
-    
-    });
-    console.log(clientsByAge);
-}
+  bellowBtn.addEventListener("click", () => {
+    const filtered = filterByAge(clients);
+    renderCards(filtered);
+  });
 
-//*REDUCE -> Reduzir - Reduz o nosso array e um único valor resultante de uma callback.
+  overBtn.addEventListener("click", () => {
+    const filtered = filterByAge(clients, true);
+    renderCards(filtered);
+  });
+};
+
 const allAge = (clientsArray) => {
-    const result = clientsArray.reduce((acumulador, currentValue) => {
-        return acumulador + currentValue.age;
-    }, 0);
+  // reduce -> reduzir - reduz o nosso array em um único valor resultante de uma callback
 
-    console.log(result / clientsArray.length);
-}
+  const result = clientsArray.reduce((acc, cur) => {
+    return acc + cur.age;
+  }, 0);
 
-//*INCLUDES -> Inclui algo - Percorre todo o array / string verificando se existe exatamente o argumento pesquisado.
+  console.log(result / clientsArray.length);
+};
+
+// includes -> inclui algo - percorre todo o array/string vereificando se existe exatamente o argumento pesquisado.
+
 const arrayTeste = [15, 24, 17, 95, 36, 88, 17, 15];
-const arrayNome = ["Fernando", "Andy", "Ale", "Chrys"]
-console.log(arrayTeste.includes(88)); //True
+const arrayNome = ["fernando", "andy", "ale", "chrys"];
+// console.log(arrayTeste.includes(88));
+// console.log(arrayNome.includes("rafa"));
 
 const frase = "O rato roeu a roupa do rei de Roma.";
-console.log(frase.includes('roeu')); //True
-console.log(frase.includes('roma')); //False
-console.log(arrayNome.includes('Ale')); //True
+// console.log(frase.toLowerCase().includes("roma"));
 
-//*INDEXOF -> Indice de algo - Percorre o array / string e retorna a posição em que foi encontrado o argumento passado.
-console.log(arrayTeste.indexOf(24)); //Indice: 1
-console.log(arrayTeste.indexOf(17, 3)); //Qual elemento que eu quero pesquisar / a partir do indice que eu passar.
-console.log(arrayTeste.indexOf(99)); // -1 pois não existe
+// indexOf -> índice de algo - percorre o array/string e torna a posição em que foi encontrado o argumento passado
 
+// console.log(arrayTeste.indexOf(24, 3));
+// console.log(frase.indexOf(" ", 2));
 
-//*SPLICE -> Emendar - Remove ou não e adiciona ou não um ou mais elementos a partir do indice indicado alterando o array inicial.
-arrayNome.splice(0, 1, 'Rafa'); //A partir do indice 0 remova 1 elemento e coloque 'Rafa' no lugar
-console.log(arrayNome);
+// splice -> emendar - remove ou não e adiciona ou não um ou mais elementos a partir do índice indicado alterando o array original
 
-arrayNome.splice(0, 0, 'Maria'); //A partir do indice 0 não remova nada e e coloque o elemento 'Maria'.
-console.log(arrayNome);
-
-const removidos = arrayNome.splice(2, 2, 'João');
-console.log(arrayNome);
-console.log(removidos);
+// const removidos = arrayNome.splice(2, 2, "rafa");
+// console.log(arrayNome);
+// console.log(removidos);
 
 renderCards(clients);
-/* listAverage(clients);
-filterByAge(clients);
-allAge(clients); */
+// listAverage(clients);
+// const filteredArray = filterByAge(clients);
+
+// renderCards(filteredArray);
+// allAge(filteredArray);
+addFilterEvents();
