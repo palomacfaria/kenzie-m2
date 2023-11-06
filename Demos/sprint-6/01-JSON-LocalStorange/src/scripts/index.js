ff7e7d433ca3abf95d61dd88c3742998ab803410
Paloma Cristina Faria <palomacfaria@outlook.com>
2023-11-06 08:11:21 -0300

    Demo Introdução JSON - LocalStorange

--- Demos/sprint-6/01-JSON-LocalStorange/src/scripts/index.js
+++ Demos/sprint-6/01-JSON-LocalStorange/src/scripts/index.js
@@ -1,3 +1,4 @@
+//*JSON
 const users = [
   {
     nome: "Carla",
@@ -43,12 +44,34 @@ const users = [
   }
 ]
 
-//STRINGFY -> Converte objeto JS para objeto JSON
+//STRINGFY -> Converte objeto JS para uma string JSON
 const arrayJson = JSON.stringify(users);
 console.log(arrayJson);
-console.log(typeof arrayJson);
+console.log(typeof arrayJson); //string
 
 //PARSE -> Converte o JSON para objeto JS
 const convertedJson = JSON.parse(arrayJson);
 console.log(convertedJson);
-console.log(typeof convertedJson);
+console.log(typeof convertedJson); //object
+
+//*LOCALSTORANGE
+//Armazenando informação
+localStorage.setItem("darkMode", true); //Se eu passar uma informação que não seja uma string, ele será convertido para string.
+
+//Buscando uma informação
+const darkMode = localStorage.getItem("darkMode");
+console.log(darkMode);
+console.log(typeof darkMode);
+
+//Enviando um objeto para o LocalStorange
+const objeto = JSON.stringify({nome: "Paloma", altura: 1.62});
+localStorage.setItem("Objeto", objeto);
+
+const objetoConvertio = localStorage.getItem("Objeto");
+
+//Transformando em Obj JS
+const objetoConvertidoJS = JSON.parse(objetoConvertio)
+console.log(objetoConvertidoJS);
+
+const texto = document.querySelector("h1");
+texto.innerText = objetoConvertidoJS.nome;
