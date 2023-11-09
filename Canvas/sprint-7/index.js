18e057f02361defb9c903ddd6f2c5063c53457a1
Paloma Cristina Faria <palomacfaria@outlook.com>
2023-11-09 19:30:41 +0000

    Assincronicidade

--- Canvas/sprint-7/index.js
+++ Canvas/sprint-7/index.js
@@ -1,1 +1,13 @@
-console.log("Async")
+function now(){
+    console.log("now");
+    return 21;
+}
+
+function later(){
+    answer = answer * 2;
+    console.log("Significado da vida", answer);
+}
+
+let answer = now();
+
+setTimeout(later, 1000);
