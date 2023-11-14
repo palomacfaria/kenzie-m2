12616c1fede6da33de3148e1195cd417a155bc77
Paloma Cristina Faria <palomacfaria@outlook.com>
2023-11-14 19:56:51 +0000

    Async await

--- Demos/sprint-7/01-API&JS-Assincrono/src/scripts/index.js
+++ Demos/sprint-7/01-API&JS-Assincrono/src/scripts/index.js
@@ -1,8 +1,17 @@
-const getArticles = () => {
-    const response = fetch("https:/api.spaceflightnewsapi.net/v4/articles");
+//Async -> Quando a função é assincrona eu estou falando para o javascript que alguma coisa dentro da minha função vai demorar
+
+//Await -> espera
+
+const  getArticles = async () => {
+    const response = await fetch(
+        "https://api.spaceflightnewsapi.net/v4/articles" //Promessa
+        )
+        .then(res => { //Pegando a resposta e transformando em json assim como no insonimia
+            return res.json();
+        })
 
     return response;
 }
 
-const result = getArticles();
+const result = await getArticles();
 console.log(result);
